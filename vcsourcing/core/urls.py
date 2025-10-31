dd50047ce1bdfe4350d945e1267d7d43a2a5facf
Desai <desai@dhcp-vl2042-11130.redrover.cornell.edu>
2025-10-31 00:29:44 -0400

    shriya ticket 2

--- vcsourcing/core/urls.py
+++ vcsourcing/core/urls.py
@@ -2,3 +2,6 @@ from django.urls import path
 from .views import health
 
 # TODO: create a list named 'urlpatterns' with the value 'path('api/health/', health),'
+urlpatterns = [
+    path('api/health/', health),
+]
