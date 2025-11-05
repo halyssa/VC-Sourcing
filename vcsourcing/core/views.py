d343acce3b3274d5ab7b0753cf7814d079da5f5f
halyssa <aahsu081405@gmail.com>
2025-11-04 21:30:04 -0500

    updated with ticket 2

--- vcsourcing/core/views.py
+++ vcsourcing/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-# TODO: complete the function below such that it uses Response to return the status "ok"
 @api_view(['GET'])
 def health(request):
-    pass
+    payload = {"status": "ok"}
+    return Response(payload)
