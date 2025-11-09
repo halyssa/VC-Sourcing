9c039c891ad07f4389ff35e71ddca625412a9a10
Desai <desai@dhcp-vl2042-11130.redrover.cornell.edu>
2025-11-09 15:19:08 -0500

    task 3

--- vcsourcing/core/urls.py
+++ vcsourcing/core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import health
+from .views import CompanyListView
 
 urlpatterns = [
      path("api/health/", health),
+]
+
+urlpatterns = [
+    path('api/companies/', CompanyListView.as_view(), name='company-list'),
 ]
--- vcsourcing/core/views.py
+++ vcsourcing/core/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Company
+from .serializers import CompanySerializer
 
 @api_view(['GET'])
 def health(request):
     payload = {"status": "ok"}
     return Response(payload)
+
+class CompanyListView(generics.ListAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name','funding_round', 'location', 'funding', 'num_employees', 'founding_year', 'growth_percentage']
