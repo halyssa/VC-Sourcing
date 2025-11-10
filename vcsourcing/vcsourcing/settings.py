8a6a9dadc8890e39b06564e3b93aa21120a1f66c
shriyadesai150-cpu <shriya.desai150@gmail.com>
2025-11-09 20:45:01 -0500

    Update settings.py
    Added REST framework settings for pagination.

--- vcsourcing/vcsourcing/settings.py
+++ vcsourcing/vcsourcing/settings.py
@@ -38,8 +38,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    'core',
+    "core"
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'ORDERING_PARAM': 'ordering',
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
