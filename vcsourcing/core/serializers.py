7f1063358b0802ad71deb8fee83fab1c13ce3e91
shriyadesai150-cpu <shriya.desai150@gmail.com>
2025-11-09 20:45:29 -0500

    Update serializers.py

--- vcsourcing/core/serializers.py
+++ vcsourcing/core/serializers.py
@@ -4,4 +4,12 @@ from .models import Company
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
-        fields = ['name', 'funding_round', 'funding', 'location', 'num_employees', 'founding_year', 'growth_percentage']
+        fields = [
+            "name",
+            "funding_round",
+            "funding",
+            "location",
+            "num_employees",
+            "founding_year",
+            "growth_percentage",
+        ]
